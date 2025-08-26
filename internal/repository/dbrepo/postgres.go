15cccfee6f7c91467c0929470e471cfc9cd4bb81
cxt314 <103610756+cxt314@users.noreply.github.com>
2025-08-26 01:13:42 +0000

    add crud db funcs for vehicles

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -9,8 +9,68 @@ import (
 
 const contextTimeout = 3 * time.Second
 
-func (m *postgresDBRepo) AllUsers() bool {
-	return true
+// AllUsers returns a slice of all users in the database
+func (m *postgresDBRepo) AllUsers() ([]models.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), contextTimeout)
+	defer cancel()
+
+	q := `SELECT first_name, last_name, email, password, access_level, created_at, updated_at
+		FROM users`
+
+	// execute our DB query
+	rows, err := m.DB.QueryContext(ctx, q)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// process query result into slice of users to return
+	var users []models.User
+
+	for rows.Next() {
+		m := models.User{}
+		err = rows.Scan(&m.ID, &m.FirstName, &m.LastName, &m.Email, &m.Password, &m.AccessLevel,
+			&m.CreatedAt, &m.UpdatedAt)
+		if err != nil {
+			return users, err
+		}
+
+		users = append(users, m)
+	}
+	err = rows.Err()
+	if err != nil {
+		return users, err
+	}
+
+	return users, nil
+}
+
+// UpdateUser updates a user in the database
+func (m *postgresDBRepo) UpdateUser(v models.User) error {
+	ctx, cancel := context.WithTimeout(context.Background(), contextTimeout)
+	defer cancel()
+
+	q := `UPDATE users SET
+			first_name = $1,
+			last_name = $2,
+			email = $3,
+			access_level = $4,
+			updated_at = $5
+		`
+
+	_, err := m.DB.ExecContext(ctx, q,
+		v.FirstName,
+		v.LastName,
+		v.Email,
+		v.AccessLevel,
+		time.Now(),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // InsertVehicle inserts a Vehicle into the database
@@ -126,4 +186,57 @@ func (m *postgresDBRepo) GetVehicleByID(id int) (models.Vehicle, error) {
 	}
 
 	return v, nil
+}
+
+// UpdateVehicle updates a vehicle in the database
+func (m *postgresDBRepo) UpdateVehicle(v models.Vehicle) error {
+	ctx, cancel := context.WithTimeout(context.Background(), contextTimeout)
+	defer cancel()
+
+	q := `UPDATE vehicles SET
+			name = $1,
+			year = $2,
+			make = $3,
+			model = $4,
+			fuel_type = $5,
+			purchase_price = $6,
+			purchase_date = $7,
+			vin = $8,
+			license_plate = $9,
+			updated_at = $10
+		`
+
+	_, err := m.DB.ExecContext(ctx, q,
+		v.Name,
+		v.Year,
+		v.Make,
+		v.Model,
+		v.FuelType,
+		v.PurchasePrice,
+		v.PurchaseDate,
+		v.Vin,
+		v.LicensePlate,
+		time.Now(),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DeleteVehicle deletes one vehicle by id
+func (m *postgresDBRepo) DeleteVehicle(id int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), contextTimeout)
+	defer cancel()
+
+	q := `DELETE from vehicles WHERE id=$1`
+
+	_, err := m.DB.ExecContext(ctx, q, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -4,8 +4,11 @@ import "github.com/cxt314/drvc-go/internal/models"
 
 type DatabaseRepo interface {
 	AllUsers() bool
+	UpdateUser(v models.User) error
 
 	InsertVehicle(v models.Vehicle) error
 	AllVehicles() ([]models.Vehicle, error)
 	GetVehicleByID(id int) (models.Vehicle, error)
+	UpdateVehicle(v models.Vehicle) error
+	DeleteVehicle(id int) error
 }
