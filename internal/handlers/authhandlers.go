edbf55417f6bd9c951ddca12c0c7bbe128b1d204
cxt314 <103610756+cxt314@users.noreply.github.com>
2025-10-31 20:54:54 +0000

    add handlers to log user in

--- internal/handlers/authhandlers.go
+++ internal/handlers/authhandlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/cxt314/drvc-go/internal/forms"
@@ -8,11 +9,47 @@ import (
 	"github.com/cxt314/drvc-go/internal/render"
 )
 
+// UserLogin shows the user login page
 func (m *Repository) UserLogin(w http.ResponseWriter, r *http.Request) {
 
 	render.Template(w, r, "login.page.tmpl", &models.TemplateData{Form: forms.New(nil)})
 }
 
+// UserLoginPost handles logging the user in
 func (m *Repository) UserLoginPost(w http.ResponseWriter, r *http.Request) {
+	_ = m.App.Session.RenewToken(r.Context())
 
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+	}
+
+	email := r.Form.Get("email")
+	password := r.Form.Get("password")
+
+	form := forms.New(r.PostForm)
+
+	// run form validations
+	form.Required("email", "password")
+	form.IsEmail("email")
+
+	if !form.Valid() {
+		render.Template(w, r, "login.page.tmpl", &models.TemplateData{
+			Form: form,
+		})
+		return
+	}
+
+	id, _, err := m.DB.Authenticate(email, password)
+	if err != nil {
+		log.Println(err)
+
+		m.App.Session.Put(r.Context(), "error", "Invalid login credentials")
+		http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+		return
+	}
+
+	m.App.Session.Put(r.Context(), "user_id", id)
+	m.App.Session.Put(r.Context(), "flash", "Logged in successfully")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
